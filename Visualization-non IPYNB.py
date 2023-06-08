6e11a56014e63cdaccb358a790d3fb740914acba
dhanudhanush2998@gmail.com <dhanudhanush2998@gmail.com>
2023-06-08 15:46:23 +0000

    Committing non IPYNB notebook with visualization

---
+++ Visualization-non IPYNB.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+sparkDF = spark.read.csv("/databricks-datasets/bikeSharing/data-001/day.csv", header="true", inferSchema="true")
+display(sparkDF)
+
+# COMMAND ----------
+
+
